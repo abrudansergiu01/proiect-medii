0a2750ff7153176e07d483f348851e3b8c717862
Sergiu Abrudan <abrudansergiu01@gmail.com>
2021-02-20 15:00:31 +0200

    updated

--- Abrudan_Sergiu/MainWindow.xaml.cs
+++ Abrudan_Sergiu/MainWindow.xaml.cs
@@ -28,239 +28,30 @@ namespace Abrudan_Sergiu
     }
     public partial class MainWindow : Window
     {
-        ActionState action = ActionState.Nothing;
-        PhoneNumbersDataSet phoneNumbersDataSet = new PhoneNumbersDataSet();
-        PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter
-        tblPhoneNumbersAdapter = new PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter();
-        Binding txtPhoneNumberBinding = new Binding();
-        Binding txtSubscriberBinding = new Binding();
-        public MainWindow()
-        {
-            InitializeComponent();
-
-            grdMain.DataContext = phoneNumbersDataSet.PhoneNumbers;
-            txtPhoneNumberBinding.Path = new PropertyPath("Phonenum");
-            txtSubscriberBinding.Path = new PropertyPath("Subscriber");
-            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
-            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
-        }
-        private void lstPhonesLoad()
-        {
-            tblPhoneNumbersAdapter.Fill(phoneNumbersDataSet.PhoneNumbers);
-        }
-        //Creem evenimentul Loaded al grid-ului de pe formular (grdMain) (fereastra Properties, modul de
-        //creere evenimente – iconiţa fulger), iar în eveniment handler-ul generat în clasa de code-behind vom
-        //apela metoda implementată mai sus
-        private void grdMain_Loaded(object sender, RoutedEventArgs e)
-        {
-            lstPhonesLoad();
-        }
-        //. Creem evenimentul Click al butonului btnExit şi adăugăm următorul cod in handlerul asociat:
-        private void btnExit_Click(object sender, RoutedEventArgs e)
-        {
-            if (MessageBox.Show("Close Application?", "Question", MessageBoxButton.YesNo,
-           MessageBoxImage.Warning) == MessageBoxResult.Yes)
-            {
-                this.Close();
-            }
-        }
-        //Creem evenimentul Loaded al formularului MainWindow în care vom încărca lista cu date:
         private void frmMain_Loaded(object sender, RoutedEventArgs e)
         {
-            PhoneNumbersDataSet phoneNumbersDataSet =
-           ((PhoneNumbersDataSet)(this.FindResource("phoneNumbersDataSet")));
-            System.Windows.Data.CollectionViewSource phoneNumbersViewSource =
-            ((System.Windows.Data.CollectionViewSource)(this.FindResource("phoneNumbersViewSource")));
+
+            Abrudan_Sergiu.PhoneNumbersDataSet1 phoneNumbersDataSet1 = ((Abrudan_Sergiu.PhoneNumbersDataSet1)(this.FindResource("phoneNumbersDataSet1")));
+            // Load data into the table Contracts. You can modify this code as needed.
+            Abrudan_Sergiu.PhoneNumbersDataSet1TableAdapters.ContractsTableAdapter phoneNumbersDataSet1ContractsTableAdapter = new Abrudan_Sergiu.PhoneNumbersDataSet1TableAdapters.ContractsTableAdapter();
+            phoneNumbersDataSet1ContractsTableAdapter.Fill(phoneNumbersDataSet1.Contracts);
+            System.Windows.Data.CollectionViewSource contractsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("contractsViewSource")));
+            contractsViewSource.View.MoveCurrentToFirst();
+            // Load data into the table PhoneNumbers. You can modify this code as needed.
+            Abrudan_Sergiu.PhoneNumbersDataSet1TableAdapters.PhoneNumbersTableAdapter phoneNumbersDataSet1PhoneNumbersTableAdapter = new Abrudan_Sergiu.PhoneNumbersDataSet1TableAdapters.PhoneNumbersTableAdapter();
+            phoneNumbersDataSet1PhoneNumbersTableAdapter.Fill(phoneNumbersDataSet1.PhoneNumbers);
+            System.Windows.Data.CollectionViewSource phoneNumbersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("phoneNumbersViewSource")));
             phoneNumbersViewSource.View.MoveCurrentToFirst();
+            // Load data into the table CommunicationsProvider. You can modify this code as needed.
+            Abrudan_Sergiu.PhoneNumbersDataSet1TableAdapters.CommunicationsProviderTableAdapter phoneNumbersDataSet1CommunicationsProviderTableAdapter = new Abrudan_Sergiu.PhoneNumbersDataSet1TableAdapters.CommunicationsProviderTableAdapter();
+            phoneNumbersDataSet1CommunicationsProviderTableAdapter.Fill(phoneNumbersDataSet1.CommunicationsProvider);
+            System.Windows.Data.CollectionViewSource communicationsProviderViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("communicationsProviderViewSource")));
+            communicationsProviderViewSource.View.MoveCurrentToFirst();
         }
-        //Creem evenimentul Click și handler-ul asociat butonului btnNew care va permite adăugarea unei noi
-        //înregistrări în baza de date:
-        private void btnNew_Click(object sender, RoutedEventArgs e)
-        {
-            action = ActionState.New;
-            btnNew.IsEnabled = false;
-            btnEdit.IsEnabled = false;
-            btnDelete.IsEnabled = false;
 
-            btnSave.IsEnabled = true;
-            btnCancel.IsEnabled = true;
-            lstPhones.IsEnabled = false;
-            btnPrevious.IsEnabled = false;
-            btnNext.IsEnabled = false;
-            txtPhoneNumber.IsEnabled = true;
-            txtSubscriber.IsEnabled = true;
-            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
-            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
-            txtPhoneNumber.Text = "";
-            txtSubscriber.Text = "";
-            Keyboard.Focus(txtPhoneNumber);
-        }
-        //. Creem evenimentul Click și handler-ul asociat butonului btnEdit care va permite editarea datelor: 
-        private void btnEdit_Click(object sender, RoutedEventArgs e)
-        {
-            action = ActionState.Edit;
-            string tempPhonenum = txtPhoneNumber.Text.ToString();
-            string tempSubscriber = txtSubscriber.Text.ToString();
-            btnNew.IsEnabled = false;
-            btnEdit.IsEnabled = false;
-            btnDelete.IsEnabled = false;
-            btnSave.IsEnabled = true;
-            btnCancel.IsEnabled = true;
-            lstPhones.IsEnabled = false;
-            btnPrevious.IsEnabled = false;
-            btnNext.IsEnabled = false;
-            txtPhoneNumber.IsEnabled = true;
-            txtSubscriber.IsEnabled = true;
-            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
-            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
-            txtPhoneNumber.Text = tempPhonenum;
-            txtSubscriber.Text = tempSubscriber;
-            Keyboard.Focus(txtPhoneNumber);
-        }
-        // Creem evenimentul Click și handler-ul asociat butonului btnDelete care va permite ștergerea datelor: 
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
-            action = ActionState.Delete;
-            string tempPhonenum = txtPhoneNumber.Text.ToString();
-            string tempSubscriber = txtSubscriber.Text.ToString();
-            btnNew.IsEnabled = false;
-            btnEdit.IsEnabled = false;
-            btnDelete.IsEnabled = false;
-            btnSave.IsEnabled = true;
-            btnCancel.IsEnabled = true;
-            lstPhones.IsEnabled = false;
-            btnPrevious.IsEnabled = false;
-            btnNext.IsEnabled = false;
-            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
-            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
-            txtPhoneNumber.Text = tempPhonenum;
-            txtSubscriber.Text = tempSubscriber;
-        }
-        // Creem evenimentul Click și handler-ul asociat butonului btnCancel care va anula operaţiile în curs de
-        // desfășurare pe baza de date: 
-        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        private void communicationsProviderDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
-            action = ActionState.Nothing;
-            btnNew.IsEnabled = true;
-            btnEdit.IsEnabled = true;
-            btnEdit.IsEnabled = true;
-            btnSave.IsEnabled = false;
-            btnCancel.IsEnabled = false;
-            lstPhones.IsEnabled = true;
-            btnPrevious.IsEnabled = true;
-            btnNext.IsEnabled = true;
-            txtPhoneNumber.IsEnabled = false;
-            txtSubscriber.IsEnabled = false;
-            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
-            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
-        }
-        //Creem evenimentul Click la butonului btnSave care va salva modificările efectuate pe date în BD:
-        private void btnSave_Click(object sender, RoutedEventArgs e)
-        {
-            if (action == ActionState.New)
-            {
-                try
-                {
-                    System.Data.DataRow newRow = phoneNumbersDataSet.PhoneNumbers.NewRow();
-                    newRow.BeginEdit();
-                    newRow["Phonenum"] = txtPhoneNumber.Text.Trim();
-                    newRow["Subscriber"] = txtSubscriber.Text.Trim();
-                    newRow.EndEdit();
-                    phoneNumbersDataSet.PhoneNumbers.Rows.Add(newRow);
-                    tblPhoneNumbersAdapter.Update(phoneNumbersDataSet.PhoneNumbers);
-                    phoneNumbersDataSet.AcceptChanges();
-                }
-                catch (System.Data.DataException ex)
-                {
-                    phoneNumbersDataSet.RejectChanges();
-                    MessageBox.Show(ex.Message);
-                }
-                btnNew.IsEnabled = true;
-                btnEdit.IsEnabled = true;
-                btnSave.IsEnabled = false;
-                btnCancel.IsEnabled = false;
-                lstPhones.IsEnabled = true;
-                btnPrevious.IsEnabled = true;
-                btnNext.IsEnabled = true;
-                txtPhoneNumber.IsEnabled = false;
-                txtSubscriber.IsEnabled = false;
-            }
-            else
-            if (action == ActionState.Edit)
-            {
-                try
-                {
-                    System.Data.DataRow editRow = phoneNumbersDataSet.PhoneNumbers.Rows[lstPhones.SelectedIndex];
-                    editRow.BeginEdit();
-                    editRow["Phonenum"] = txtPhoneNumber.Text.Trim();
-                    editRow["Subscriber"] = txtSubscriber.Text.Trim();
-                    editRow.EndEdit();
-                    tblPhoneNumbersAdapter.Update(phoneNumbersDataSet.PhoneNumbers);
-                    phoneNumbersDataSet.AcceptChanges();
-                }
-                catch (System.Data.DataException ex)
-                {
-                    phoneNumbersDataSet.RejectChanges();
-                    MessageBox.Show(ex.Message);
-                }
-                btnNew.IsEnabled = true;
-                btnEdit.IsEnabled = true;
-                btnDelete.IsEnabled = true;
-                btnSave.IsEnabled = false;
-                btnCancel.IsEnabled = false;
-                lstPhones.IsEnabled = true;
-                btnPrevious.IsEnabled = true;
-                btnNext.IsEnabled = true;
-                txtPhoneNumber.IsEnabled = false;
-                txtSubscriber.IsEnabled = false;
-                txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
-                txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
-            }
-            else
-            if (action == ActionState.Delete)
-            {
-                try
-                {
-                    System.Data.DataRow deleterow = phoneNumbersDataSet.PhoneNumbers.Rows[lstPhones.SelectedIndex];
-                    deleterow.Delete();
-
-                    tblPhoneNumbersAdapter.Update(phoneNumbersDataSet.PhoneNumbers);
-                    phoneNumbersDataSet.AcceptChanges();
-                }
-                catch (System.Data.DataException ex)
-                {
-                    phoneNumbersDataSet.RejectChanges(); MessageBox.Show(ex.Message);
-                    MessageBox.Show(ex.Message);
-                }
-                btnNew.IsEnabled = true;
-                btnEdit.IsEnabled = true;
-                btnDelete.IsEnabled = true;
-                btnSave.IsEnabled = false;
-                btnCancel.IsEnabled = false;
-                lstPhones.IsEnabled = true;
-                btnPrevious.IsEnabled = true;
-                btnNext.IsEnabled = true;
-                txtPhoneNumber.IsEnabled = false;
-                txtSubscriber.IsEnabled = false;
-                txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
-                txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
-            }
-        }
-        //Creem evenimentul Click al butonului btnPrevious şi adăugăm următorul cod în handler: 
-        private void btnPrevious_Click(object sender, RoutedEventArgs e)
-        {
-            //using System.ComponentModel
-            ICollectionView navigationView =
-           CollectionViewSource.GetDefaultView(phoneNumbersDataSet.PhoneNumbers);
-            navigationView.MoveCurrentToPrevious();
-        }
-        // Creem evenimentul Click al butonului btnNext şi adăugăm următorul cod în handler-ul asociat:
-        private void btnNext_Click(object sender, RoutedEventArgs e)
-        {
-            ICollectionView navigationView =
-           CollectionViewSource.GetDefaultView(phoneNumbersDataSet.PhoneNumbers);
-            navigationView.MoveCurrentToNext();
         }
     }
 }
